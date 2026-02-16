2f6741812d568ff72ab8051e10d01edc5f32d27b
brandon mazey <brandon@brandons-MacBook-Air.local>
2026-02-16 10:08:20 -0500

    baseline

--- src/main/java/Detector.java
+++ src/main/java/Detector.java
@@ -1,5 +1,5 @@
 public class Detector {
     public static void main(String[] args) {
-        System.out.println("new dawn, new day")
+        System.out.println("new dawn, new day");
     }
 }
---
+++ src/test/java/DetectorTest.java
@@ -0,0 +1,5 @@
+package test.java;
+
+public class DetectorTest {
+    // TODO Implement tests!
+}
